d529965a70e20934a46ac8d972691fc55b050bfd
Prathamesh Pokhare <64691299+KarlWardin27@users.noreply.github.com>
2020-09-19 13:23:57 +0530

    Modified according to PEP8 Coding Standard

--- Codes/Augmentation/Augmentation.py
+++ Codes/Augmentation/Augmentation.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
+
 import cv2
 import numpy as np
 import shutil
 import imutils
 import pandas as pd 
 
+
 def main():
-    data = pd.read_csv("D:/Code_Me/Python/Bone_Age_Detection/TrainData/datasets_10832_15122_boneage-training-dataset.csv")
-    Path = 'D:/Code_Me/Python/Bone_Age_Detection/TrainData/PNGFormat/'
-    ResultPath = 'D:/Code_Me/Python/Bone_Age_Detection/Augmented data/Train Images/'
+
+    #For Image 
+ 
+    ORIGINAL_IMAGE_PATH = 'D:/Code_Me/Python/Bone_Age_Detection/TrainData/PNGFormat/'
+    RESULT_IMAGE_PATH = 'D:/Code_Me/Python/Bone_Age_Detection/Augmented data/Train Images/'
     counter = 1
     csvcount = 0
-    # 15611
 
-    #For CSV File
+    #For Creating a Modified CSV File
+
+    ORIGINAL_DATA = pd.read_csv("D:/Code_Me/Python/Bone_Age_Detection/TrainData/datasets_10832_15122_boneage-training-dataset.csv")
     df = pd.DataFrame(
         {
             "id":[ ],
@@ -20,19 +26,25 @@ def main():
             "male":[ ]
         }
     )
+
+    #Converting Datatype of "male" column to Boolean
     convert_dict = {'male': bool, 
                 } 
     df = df.astype(convert_dict)
 
-    for x in range(1377, 1478):
-        Path1 = Path + str(x) + '.png'
-        ResultPath1 = ResultPath + str(counter) + '.png'
+    #Augments Images named 1377.png all the way upto 15611.png, by rotating images by 10 degrees from -90 to 90   
+    for x in range(1377, 15611):
+
+        Path = ORIGINAL_IMAGE_PATH + str(x) + '.png'
+        Result_Path = RESULT_IMAGE_PATH + str(counter) + '.png'
+
+        #A Try-Catch Block used to make the algorithm work even if there is a missing image name. 
         try:
-            shutil.copyfile(Path1, ResultPath1)  
-            image = cv2.imread(Path1)
+            shutil.copyfile(Path, Result_Path)  
+            image = cv2.imread(Path)
             df.at[counter-1, 'id'] = counter
-            df.at[counter-1, 'boneage'] = data['boneage'][csvcount]
-            df.at[counter-1, 'male'] = data['male'][csvcount]
+            df.at[counter-1, 'boneage'] = ORIGINAL_DATA['boneage'][csvcount]
+            df.at[counter-1, 'male'] = ORIGINAL_DATA['male'][csvcount]
              
             for y in range(-90,91,10):
                 
@@ -42,11 +54,11 @@ def main():
                 else:
                     counter = counter + 1
                     df.at[counter-1, 'id'] = counter
-                    df.at[counter-1, 'boneage'] = data['boneage'][csvcount]
-                    df.at[counter-1, 'male'] = data['male'][csvcount]
+                    df.at[counter-1, 'boneage'] = ORIGINAL_DATA['boneage'][csvcount]
+                    df.at[counter-1, 'male'] = ORIGINAL_DATA['male'][csvcount]
                     rotated = imutils.rotate_bound(image, y)
-                    name = ResultPath +str(counter)+ ".png"
-                    cv2.imwrite(name, rotated)
+                    Temp_String_Name = RESULT_IMAGE_PATH +str(counter)+ ".png"
+                    cv2.imwrite(Temp_String_Name, rotated)
                                   
         except:
             continue
@@ -54,8 +66,10 @@ def main():
         counter = counter + 1     
         csvcount = csvcount + 1
         print(str(x) + " of 15611 " + str(counter))
-
+    
+    #Saving the CSV file
     df.to_csv("D:/Code_Me/Python/Bone_Age_Detection/Augmented data/AugCSV.csv")
+
 
 if __name__ == "__main__":
     main()
