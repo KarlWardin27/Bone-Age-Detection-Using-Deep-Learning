910f6176745d804608498db16c7c6a15a0953091
KarlWardin27 <prathamesh.gp@somaiya.edu>
2021-03-22 18:20:40 +0530

    Added DICOM to PNG convertion code

--- Codes/Image Conversion/DICOM_to_PNG.py
+++ Codes/Image Conversion/DICOM_to_PNG.py
@@ -1,15 +1,20 @@
 import cv2
 import os
-import pydicom 
+import pydicom #Convert DICOM images to PNG.
 
+#Directory of the Images.
 inputdir = 'C:/New folder/'
+
+#Output Directory for PNG images
 outdir = 'D:/Code_Me/Python/Bone_Age_Detection'
 #os.mkdir(outdir)
 
 test_list = [ f for f in  os.listdir(inputdir)]
 
 for f in test_list: 
-    ds = pydicom.read_file(inputdir + f) # read dicom image
-    img = ds.PixelData   # get image array
+    # read dicom image
+    ds = pydicom.read_file(inputdir + f) 
+
+     # get image array
+    img = ds.PixelData
     cv2.imwrite(f.replace('.DCM','.png'),img)
-    #ds.save_as("Try.png")
