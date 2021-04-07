7785f354357b8b3f7b92a6acbc046befa14d9d1e
KarlWardin27 <prathamesh.gp@somaiya.edu>
2021-04-07 21:42:32 +0530

    Fixed Choosing and Loading of Image with other minor changes

--- Web App Development/app.py
+++ Web App Development/app.py
@@ -1,8 +1,11 @@
+#Authors:- Pratik Poojary, Prathamesh Pokhare
+
 from flask import Flask, render_template, request
 import tensorflow as tf
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 from keras_preprocessing import image
 import numpy as np
-import cv2
 import matplotlib.pyplot as plt
 import itertools
 import threading
@@ -23,22 +26,22 @@ def home():
     
     img = request.files['image']
 
-    model = tf.keras.models.load_model('best_model.h5', compile=False)
-    #prediction = model.predict(img_arr)
-    img=tf.keras.preprocessing.image.load_img(img,target_size=(256,256))
+    img.save(secure_filename(img.filename))
+    img = secure_filename(img.filename)
 
+    model = tf.keras.models.load_model('best_model.h5', compile=False)
+
+    img=tf.keras.preprocessing.image.load_img(img,target_size=(256,256))
     img=tf.keras.preprocessing.image.img_to_array(img)
     img=tf.keras.applications.xception.preprocess_input(img)
-    mean_bone_age = 127.3207517246848
-    std_bone_age = 41.18202139939618
 
-    pred = mean_bone_age + std_bone_age*(model.predict(np.array([img])))
- 
-    '''x = round(prediction[0,0], 2)
-    y = round(prediction[0,1], 2)
-    preds = np.array([x,y])
-    COUNT += 1'''
-    return render_template('prediction.html', data=pred)    
+    #Calculated for the Model Training Part. Will vary with Training DataSet.
+    Mean_Bone_Age = 127.3207517246848
+    Standard_Bone_Age = 41.18202139939618
+
+    Predict = Mean_Bone_Age + Standard_Bone_Age*(model.predict(np.array([img])))
+
+    return render_template('prediction.html', data = Predict)    
 
 if __name__=='__main__':
     app.run(debug=True)   
